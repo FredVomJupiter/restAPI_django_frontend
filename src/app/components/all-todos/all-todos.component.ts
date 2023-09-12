df0e28c371ec88e90639853b9e54c1eb72e72029
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-12 16:21:33 +0300

    implemented Login & Logout

--- src/app/components/all-todos/all-todos.component.ts
+++ src/app/components/all-todos/all-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-all-todos',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-todos.component.scss']
 })
 export class AllTodosComponent {
+
+  constructor(private router: Router) { }
+
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,16 +8,55 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  @ViewChild('errormessage') errormessage!: HTMLElement;
+  username: string = '';
+  password: string = 'Error';
+  error: string = '';
+  visible: boolean = false;
 
-  email: string = '';
-  password: string = '';
-
-  constructor() { }
+  constructor(private router: Router) { }
 
 
-  login() {
-    console.log('login');
-    console.log('email:', this.email);
-    console.log('password:', this.password);
+  async loginWithUsernameAndPassword() {
+    const requestOptions = this.getSettings();
+
+    try {
+      let resp = await fetch("http://127.0.0.1:8000/login/", requestOptions);
+      let data = await resp.json();
+      localStorage.setItem('token', data.token);
+      if (data.token === undefined) {
+        this.error = 'Wrong username or password';
+        this.visible = true;
+        setTimeout(() => {
+          this.visible = false;
+        }, 3000);
+        throw new Error('No token received');
+      } else {
+      this.router.navigate(['/alltodos']);
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  getSettings() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "username": this.username,
+      "password": this.password
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    return requestOptions;
   }
 }
