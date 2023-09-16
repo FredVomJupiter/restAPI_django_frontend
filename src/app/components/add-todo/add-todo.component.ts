f6e5f206ededb1386b3cbd7fa5eaa8ba11f31465
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-16 21:15:41 +0300

    refactored add-todo

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,10 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
 import { OverlayService } from 'src/app/services/overlay.service';
-import { environment } from 'src/environments/environment';
 
 interface Todo {
   id: number;
@@ -27,6 +25,8 @@ interface Todo {
 export class AddTodoComponent {
 
   categories!: string[];
+  categoryFormVisible: boolean = false;
+  
 
   title: string = '';
   description: string = '';
@@ -37,7 +37,7 @@ export class AddTodoComponent {
     title: new FormControl(this.title, Validators.required),
     description: new FormControl(this.description, Validators.required),
     completed: new FormControl(this.status),
-    category: new FormControl(this.category, Validators.required),
+    category: new FormControl(this.category, Validators.required)
   });
 
   youShallNotPass: boolean = true;
@@ -45,38 +45,28 @@ export class AddTodoComponent {
   constructor(
     public oS: OverlayService,
     private router: Router,
-    private http: HttpClient
+    private dataService: DataService
     ) { }
 
 
   async ngOnInit(): Promise<void> {
-    this.categories = await this.getCategories().then((data) => {
+    this.categories = await this.dataService.getCategories().then((data) => {
       return data;
     });
   }
 
 
-  async getCategories() {
-    const url = environment.baseUrl + '/api/v1/categories/';
-    return await lastValueFrom(this.http.get<any>(url));
-  }
-
-
-  createCategory() {
-    console.log('create category');
-    
-    console.log(this.category);
+  openCategoryForm() {
+    this.category = '';
+    this.oS.categoryOverlayVisible = true;
   }
 
 
   async create() {
     if (this.todoForm.valid) {
-      console.log(this.todoForm.value);
-      const url = environment.baseUrl + '/api/v1/todos/';
-      await lastValueFrom(this.http.post<any>(url, this.todoForm.value));
-      this.oS.setObservableTrue();
+      this.dataService.createTodo(this.todoForm.value as Todo)
+      this.oS.setSubjectTrue();
       this.closeForm();
-      
     } else {
       console.log('invalid form');
     }
@@ -93,6 +83,7 @@ export class AddTodoComponent {
   noBubble($event: any) {
     $event.stopPropagation();
   }
+
 
   checkForm() {
     if (this.todoForm.valid) {
